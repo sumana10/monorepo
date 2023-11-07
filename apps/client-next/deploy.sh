5fa717bec7a26bb70c0ac5a7457bc68d49700cc1
sumana10 <sumanaonlyme@gmail.com>
2023-11-07 17:18:40 +0530

    update deploy.sh

--- apps/client-next/deploy.sh
+++ apps/client-next/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v21.1.0/bin
 
-cd /home/ubuntu/week-12-monorepo-full
+cd /home/ubuntu/monorepo
 git pull origin master
 yarn build
 pm2 stop next
