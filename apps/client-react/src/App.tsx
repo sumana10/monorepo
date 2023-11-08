6277c551c5e538f00dc19c9e65a5b71a44424917
sumana10 <sumanaonlyme@gmail.com>
2023-11-08 11:49:19 +0530

    update react app

--- apps/client-react/src/App.tsx
+++ apps/client-react/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Minds are like parachutes. They only function when open.</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
